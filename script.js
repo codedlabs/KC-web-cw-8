bea3128ecea85adb42410a09248488dd72d794fb
Waleed <62436128+WaliiidAls@users.noreply.github.com>
2022-06-26 10:20:19 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+let grades = [90, 94, 65, 80, 39, 55, 83, 92, 39, 93, 43, 70, 57, 0];
+
+let all_grades_div = document.getElementById("all-grades");
+let search_grades_div = document.getElementById("search-grades");
+let A_grades_div = document.getElementById("A-grades");
+let bonus_grades_div = document.getElementById("bonus-grades");
+let random_grade_div = document.getElementById("random-grade");
+
+
+function search() {
+  let inputValue = document.getElementById("input").value;
+  
+}
