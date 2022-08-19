f96a3872175dc304a2718badfe21b22a3fbcb7eb
AnasMarzouq <63076945+AnasMeshal@users.noreply.github.com>
2022-08-20 02:46:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,25 @@ let A_grades_div = document.getElementById("A-grades");
 let bonus_grades_div = document.getElementById("bonus-grades");
 let random_grade_div = document.getElementById("random-grade");
 
+// لا تقم بتغيير او تخريب الكود الموجود فوق هذا الكومنت
+
+// طبّق الخطوة 4 في أسفل هذا الكومنت
+
+// طبّق الخطوة 5 في أسفل هذا الكومنت
+
+// طبّق الخطوة 6 أسفل هذا الكومنت
+
+
 
 function search() {
   let inputValue = document.getElementById("input").value;
   
+  // طبّق الخطوة 7 أسفل هذا الكومنت
+  
 }
+
+// طبّق الخطوة 8 و 9 أسفل هذا الكومنت
+
+// طبّق الخطوة 10 أسفل هذا الكومنت
+
+
