a564bd5caf79496e471a484d398d09c0c9614283
Mohammed Buzlouf <66552256+Mbuzlouf@users.noreply.github.com>
2022-11-02 18:26:28 +0300

    Good Luck Web Students!! 🔥

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let random_grade_div = document.getElementById("random-grade");
 
 // طبّق الخطوة 5 في أسفل هذا الكومنت
 
-// طبّق الخطوة 6 أسفل هذا الكومنت
+// طبّق الخطوة 6 أسفل  هذا الكومنت
 
 
 
